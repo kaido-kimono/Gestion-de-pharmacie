697aafaeb62af3234bfe41c80b9cecb715087a53
Mayeze Showee <showeemayeze@outlook.fr>
2019-07-14 19:59:40 +0200

    Ajout de la classe Operations

---
+++ bibliothequeGestionPharmacie/Operations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bibliothequeGestionPharmacie
+{
+	public class Operations
+	{
+        public int IDoperation { get; private set; }
+        private static int _tour = 0;
+
+
+	}
+}
